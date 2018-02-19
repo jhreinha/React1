64f1af908216e97d2119da121730e340465fac23
jhreinha <joereinhart@shaw.ca>
2018-02-19 13:35:53 -0700

    up to redux

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,18 +4,22 @@ class SearchBar extends Component {
     constructor(props){
         super(props);
 
-        this.state = { term: 'prepop bitch'};
+        this.state = { term: '' };
     }
 
     render() {
-        return(
-            <div>
-                <input 
+        return (
+            <div className="search-bar">
+                <input
                     value={this.state.term}
-                    onChange={event => this.setState({ term: event.target.value })} />;
-                Value of the input: {this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)} />;
             </div>
         );
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
